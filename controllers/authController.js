03f7365ec993af03224b58f3fb3002815f942323
hamzakhanx64 <hamzakhattak888@gmail.com>
2026-03-10 19:00:54 +0500

    replace bcrypt with bcryptjs for vercel

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // signup() — Register a new user
 exports.signup = async (req, res) => {
